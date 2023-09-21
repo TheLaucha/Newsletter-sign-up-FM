2bd7dcbe3818c248ec5d308ce8fdfdeb7a1405fa
Lautaro <espinillo.lautaro@gmail.com>
2023-09-21 10:03:15 -0300

    added

---
+++ script.js
@@ -0,0 +1,50 @@
+// Cambiar imagen al disminuir tamaño de pantalla
+
+window.addEventListener("resize", changeImage)
+window.addEventListener("load", changeImage)
+
+const containerImage = document.querySelector(".containerImage")
+
+function changeImage() {
+  var widthWindow = window.innerWidth
+  console.log("entra")
+  if (widthWindow < 768) {
+    containerImage.src = "./assets/images/illustration-sign-up-mobile.svg"
+  } else {
+    containerImage.src = "./assets/images/illustration-sign-up-desktop.svg"
+  }
+}
+
+// Manejar envio de formulario
+
+const form = document.querySelector(".form")
+const containerForm = document.querySelector(".containerForm")
+const containerMessage = document.querySelector(".containerMessage")
+const btnMessage = document.querySelector(".btnMessage")
+const email = document.querySelector("#email")
+const emailTo = document.querySelector(".emailTo")
+
+form.addEventListener("submit", showMessage)
+btnMessage.addEventListener("click", showForm)
+
+function showMessage(e) {
+  e.preventDefault()
+  let validEmail = false
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
+    validEmail = true
+  }
+
+  if (validEmail) {
+    containerForm.style.display = "none"
+    containerMessage.style.display = "flex"
+    emailTo.textContent = email.value
+    form.reset()
+  } else {
+    email.classList.add("invalid")
+  }
+}
+
+function showForm() {
+  containerForm.style.display = "flex"
+  containerMessage.style.display = "none"
+}
